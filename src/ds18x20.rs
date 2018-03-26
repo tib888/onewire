14177f59d446ea510e3e3762c836855535eda5aa
tib <none@none>
2018-03-26 21:21:23 +0200

    initial working version, but the 'atomic' delay from falling edges is missing.

---
+++ src/ds18x20.rs
@@ -0,0 +1,94 @@
+//! Read the temperature from DS18B20 1-wire temperature sensors connected to B6 GPIO
+
+#![deny(unsafe_code)]
+//#![deny(warnings)]
+
+use OneWire;
+use PortErrors;
+use calculate_crc;
+
+pub enum DS18x20Devices {
+    DS18S20, // or old DS1820
+    DS18B20,
+    DS1822,
+}
+
+pub trait DS18x20 {
+    fn start_temperature_measurement(&mut self, rom: &[u8; 8]) -> Result<u16, PortErrors>;
+    fn read_temperature_measurement_result(&mut self, rom: &[u8; 8]) -> Result<u16, PortErrors>;
+}
+
+pub fn detect_18x20_devices(factory_code: u8) -> Option<DS18x20Devices> {
+    match factory_code {
+        0x10 => Some(DS18x20Devices::DS18S20),  // or old DS1820
+        0x28 => Some(DS18x20Devices::DS18B20),
+        0x22 => Some(DS18x20Devices::DS1822),
+        _ => None,
+    }
+}
+
+impl<T: OneWire> DS18x20 for T {
+    /// Measure the temperature with 18x20, returns temp in celsius * 16
+    /// returs the time in ms required for conversion
+    fn start_temperature_measurement(&mut self, rom: &[u8; 8]) -> Result<u16, PortErrors> {
+        
+        if let Err(error) = self.reset() {
+            return Err(error);
+        }
+
+        self.select(&rom);
+
+        //start conversion
+        self.send_byte(0x44);
+
+        //with parasite power ON
+        //self.strong_pullup(true);
+
+        Ok(800) //TODO may shorten this if lower bit resolution was chosen.
+    }
+
+    fn read_temperature_measurement_result(&mut self, rom: &[u8; 8]) -> Result<u16, PortErrors> {
+        //with parasite power OFF
+        //self.strong_pullup(false);
+
+        if let Err(err) = self.reset() {
+            return Err(err);
+        }
+
+        self.select(&rom);
+
+        // Read Scratchpad
+        self.send_byte(0xBE);
+
+        let mut scratchpad = [0u8; 9];
+        self.request_many(&mut scratchpad[..]);
+
+        if calculate_crc(&scratchpad[..]) == 0 {
+            let mut rawtemp: u16 = scratchpad[0] as u16 | ((scratchpad[1] as u16) << 8);
+
+            if rom[0] == 0x10 {
+                //DS18S20 or old DS1820
+                rawtemp <<= 3; // 9 bit resolution default
+                //[7] = count per celsius
+                //[6] = remaining count
+                //t = temp_read - 0.25 + (count_pre_celsius-remaining_count)/count_pre_celsius
+                if scratchpad[7] == 0x10 {
+                    // "count remain" gives full 12 bit resolution
+                    rawtemp = (rawtemp & 0xFFF0) - 4u16 + 16u16 - (scratchpad[6] as u16);
+                }
+            } else {
+                // at lower res, the low bits are undefined, so let's zero them
+                rawtemp &= match scratchpad[4] & 0x60 {
+                    0x00 => !7,  // 9 bit resolution, 93.75 ms
+                    0x20 => !3,  // 10 bit res, 187.5 ms
+                    0x40 => !1,  // 11 bit res, 375 ms
+                    _ => !0,     // default is 12 bit resolution, 750 ms conversion time
+                }
+            }
+
+            Ok(rawtemp) //celsius = rawtemp/16.0
+        } else {
+            Err(PortErrors::CRCMismatch)
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,353 @@
+#![no_std]
+
+extern crate embedded_hal as hal;
+//extern crate cortex_m;
+
+pub mod ds18x20;
+
+use hal::digital::{OutputPin, InputPin};
+use hal::blocking::delay::{DelayUs};
+//use cortex_m::interrupt::{self};
+
+///generic 1-wire API:
+
+#[derive(Debug)]
+pub enum PortErrors {
+    ShortDetected,          //line stays low - probably pullup resistor missing
+    NoPresencePulseDetect,  //no devices detected at reset
+    NoDevices,              //rom enumeration does not see any devices
+    CRCMismatch,            //crc check fails
+}
+
+pub type Rom = [u8; 8];
+
+pub struct RomIterator {
+    last_device_flag: bool,
+    last_discrepancy: i32,
+    last_family_discrepancy: i32,
+    rom: Rom,
+}
+
+impl RomIterator {
+    /// Setup the search to find the device type 'family_code' on the next call
+    /// to search(...) if it is present.
+    /// family code = 0 will work on all devices
+    pub fn new(family_code: u8) -> RomIterator {
+        // set the search state to find SearchFamily type devices
+        RomIterator {
+            last_discrepancy: 0,
+            last_device_flag: false,
+            last_family_discrepancy: 0,
+            rom: [family_code, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8],
+        }
+    }
+
+    //reset counters so next 'search' will be like a first
+    pub fn reset(&mut self, family_code: u8) {
+        self.last_discrepancy = 0;
+        self.last_device_flag = false;
+        self.last_family_discrepancy = 0;
+        self.rom[0] = family_code;
+    }
+}
+
+// Compute a Dallas Semiconductor 8 bit CRC directly.
+// this is much slower, but much smaller, than the lookup table.
+pub fn calculate_crc(buffer: &[u8]) -> u8 {
+    let mut crc = 0;
+    for b in buffer.iter() {
+        let mut d = *b as u32;
+        for _ in 0..8 {
+            if (crc ^ d) & 1 != 0 {
+                crc >>= 1;
+                crc ^= 0x8C;
+            } else {
+                crc >>= 1;
+            }
+            d >>= 1;
+        }
+    }
+    (crc as u8)
+}
+
+pub trait OneWire {
+    fn reset(&mut self) -> Result<(), PortErrors>;
+    fn send_byte(&mut self, data: u8);
+    fn request_byte(&mut self) -> u8;
+    
+    fn send_many(&mut self, data: &[u8]) {
+        for d in data {
+            self.send_byte(*d);
+        }
+    }
+
+    /// fills the buffer with bytes coming on the 1 wire bus
+    fn request_many(&mut self, buffer: &mut [u8]) {
+        for d in buffer.iter_mut() {
+            *d = self.request_byte();
+        }
+    }
+
+    /// Do a ROM select
+    fn select(&mut self, rom: &Rom) {
+        // Choose ROM
+        self.send_byte(0x55);        
+        self.send_many(&rom[..]);
+    }
+
+    /// Do a ROM skip
+    fn skip(&mut self) {
+        // Skip ROM
+        self.send_byte(0xCC);
+    }
+
+    /// Perform the 1-Wire search algorithm on the 1-Wire bus using the iterator state
+    /// normal_search_mode : true = normal search, false = alarm/conditional
+    fn iterate_next<'a>(&mut self, normal_search_mode: bool, it: &'a mut RomIterator) -> Result<Option<&'a Rom>, PortErrors>;
+}
+
+/// A 1-Wire porotocol bit banging implementation
+pub struct OneWirePort<OPIN, IPIN, DELAY>
+where
+    OPIN: OutputPin,//in opendrain mode
+    IPIN: InputPin,//in opendrain mode the pin also acts as floating input
+    DELAY: DelayUs<u16>
+{
+    /// an external 4.7k pullup resistor is needed on this pin
+    output: OPIN,  
+    input: IPIN,
+    delay: DELAY
+
+    //TODO add support for a strong pullup pin
+}
+
+/// Executes the closure `f` in a preemption free context
+/// During the execution of the closure no task can preempt the current task.
+/// use this only where the timing is critical, otherwise shall not block.
+fn atomic<R, F>(f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    //unsafe { interrupt::disable(); };//TODO
+    let r = f();
+    //unsafe { interrupt::enable(); };//TODO
+    r
+}
+
+const DELAY_CALIBRATION : u16 = 3u16;
+
+impl<OPIN, IPIN, DELAY> OneWirePort<OPIN, IPIN, DELAY>
+where
+    OPIN: OutputPin,
+    IPIN: InputPin,
+    DELAY: DelayUs<u16>
+{
+    /// Creates a new `Motor`
+    pub fn new(mut output: OPIN, input: IPIN, delay: DELAY) -> Self {
+        // initial state: hi
+        //TODO pin.into_open_drain();
+        output.set_high();
+        OneWirePort {
+            output: output,
+            input: input,
+            delay: delay,
+        }
+    }
+
+    fn send_bit(&mut self, data: bool) {
+        //the slave will sample the line 15..60us from the initial falling edge
+        //TODO instead of delay_ticks read the counter first then wait until relative positions     
+        if data {
+            //short low pulse = 1
+            atomic(|| {
+                self.output.set_low();
+                self.delay.delay_us(9u16 - DELAY_CALIBRATION);
+                self.output.set_high();
+            });
+            self.delay.delay_us(80u16 - 9u16 - DELAY_CALIBRATION);
+        } else {
+            //long low pulse = 0
+            atomic(|| {
+                self.output.set_low();
+                self.delay.delay_us(60u16 - DELAY_CALIBRATION);
+                self.output.set_high();
+            });
+            self.delay.delay_us(80u16 - 60u16 - DELAY_CALIBRATION);
+        }
+    }
+
+    fn request_bit(&mut self) -> bool {
+        //the master will sample the line 15us from the initial falling edge
+        //TODO instead of delay_ticks read the counter first then wait until relative positions
+        {
+            let result = 
+            atomic(|| {
+                //send out a short low pulse
+                self.output.set_low();
+                self.delay.delay_us(9u16 - DELAY_CALIBRATION);
+                self.output.set_high();
+                self.delay.delay_us(9u16 - DELAY_CALIBRATION);//6?
+
+                //then sample the port if a slave keeps it pulled down at 15us
+                self.input.is_high()
+            });
+
+            self.delay.delay_us(80u16 - 9u16 - 9u16 - DELAY_CALIBRATION);
+            result
+        }
+    }
+}
+
+impl<OPIN, IPIN, DELAY> OneWire for OneWirePort<OPIN, IPIN, DELAY>
+where
+    OPIN: OutputPin,//in opendrain mode
+    IPIN: InputPin,//in opendrain mode
+    DELAY: DelayUs<u16>,
+{
+    fn reset(&mut self) -> Result<(), PortErrors> {
+        //test few times if the wire is high... just in case
+        let mut retry = 125;
+        loop {
+            retry -= 1;
+            if retry == 0 {
+                return Err(PortErrors::ShortDetected);
+            }
+            if self.input.is_high() {
+                break;
+            }
+            self.delay.delay_us(2u16 - DELAY_CALIBRATION);
+        }
+
+        //TODO instead of delay_ticks read the counter first then wait until relative positions
+        let device_present = {
+            //long (480..640us) low reset pulse:
+            self.output.set_low();
+            self.delay.delay_us(480u16 - DELAY_CALIBRATION);
+            atomic(|| {
+                self.output.set_high();
+                //wait 15..60us - external pullup brings the line high 
+                //then sample the line if any device pulls it down to show its presence
+                self.delay.delay_us(72u16 - DELAY_CALIBRATION);
+                self.input.is_low()
+            })
+        };
+
+        //then wait for recovery at least 100..180us
+        self.delay.delay_us(320u16 - DELAY_CALIBRATION);
+        //frame ends here
+
+        if device_present {
+            Ok(())
+        } else {
+            Err(PortErrors::NoPresencePulseDetect)
+        }
+    }
+
+    fn send_byte(&mut self, data: u8) {
+        for i in 0..8 {
+            self.send_bit(data & (1 << i) != 0);
+        }
+    }
+
+    fn request_byte(&mut self) -> u8 {
+        let mut result: u8 = 0u8;
+        for i in 0..8 {
+            if self.request_bit() {
+                result |= 1 << i;
+            }
+        }
+        result
+    }
+
+    fn iterate_next<'a>(&mut self, normal_search_mode: bool, it: &'a mut RomIterator) -> Result<Option<&'a Rom>, PortErrors> {
+        if let Err(error) = self.reset() {
+            return Err(error);
+        } else {
+            if !it.last_device_flag {
+                let mut id_bit_number = 1;
+                let mut id_byte_number = 0;
+                let mut id_byte_mask = 1u8;
+                let mut last_zero = 0;
+
+                // issue the search command
+                if normal_search_mode {
+                    self.send_byte(0xF0); // NORMAL SEARCH
+                } else {
+                    self.send_byte(0xEC); // CONDITIONAL SEARCH
+                }
+
+                loop {
+                    // read a bit and its complement
+                    let id_bit = self.request_bit();
+                    let cmp_id_bit = self.request_bit();
+
+                    // check for no devices on 1-wire
+                    if id_bit && cmp_id_bit {
+                        return Err(PortErrors::NoDevices);
+                    }
+
+                    let search_direction = if id_bit || cmp_id_bit {
+                        // id_bit != cmp_id_bit means
+                        // all devices coupled have 0 or 1
+                        id_bit
+                    } else {
+                        // if this discrepancy if before the Last Discrepancy
+                        // on a previous next then pick the same as last time
+                        let dir = if id_bit_number < it.last_discrepancy {
+                            (it.rom[id_byte_number] & id_byte_mask) != 0
+                        } else {
+                            id_bit_number == it.last_discrepancy
+                        };
+
+                        // if 0 was picked then record its position in LastZero
+                        if !dir { 
+                            last_zero = id_bit_number;
+
+                            // check for Last discrepancy in family
+                            if last_zero < 9 {
+                                it.last_family_discrepancy = last_zero;
+                            }
+                        }
+
+                        dir
+                    };
+
+                    // set or clear the bit in the ROM byte rom_byte_number
+                    // with mask rom_byte_mask
+                    if search_direction {
+                        it.rom[id_byte_number] |= id_byte_mask;
+                    } else {
+                        it.rom[id_byte_number] &= !id_byte_mask;
+                    }
+                    self.send_bit(search_direction);
+
+                    id_bit_number += 1;
+                    id_byte_mask <<= 1;
+                    if id_byte_mask == 0 {
+                        id_byte_number += 1;
+                        id_byte_mask = 1;
+                    }
+
+                    // loop until through all ROM bytes 0-7
+                    if id_byte_number >= 8 {
+                        it.last_discrepancy = last_zero;
+
+                        if it.last_discrepancy == 0 {
+                            it.last_device_flag = true;
+                        }
+
+                        if 0 != calculate_crc(&it.rom[..]) {
+                            return Err(PortErrors::CRCMismatch);
+                        }
+
+                        // if the search was successful then
+                        return Ok(Some(&it.rom));
+                    }                    
+                }
+            }
+        }
+
+        // if no (or no more) device found
+        Ok(None)
+    }
+}
