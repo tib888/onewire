fca47b4d928f435bf6f0064416aab3f6e48eddbd
tib888 <pro@sch.bme.hu>
2018-09-11 15:36:40 +0200

    revert previous

--- src/temperature.rs
+++ src/temperature.rs
@@ -15,42 +15,8 @@ impl Temperature {
         self.0 >> 4
     }
 
-    pub fn fraction_degrees(&self) -> i16 {
-        if self.0 >= 0 {
-            self.0 & 0xF
-        } else {
-            (-self.0) & 0xF
-        }
-    }
-
-    //format a temperature given in 1/16 celsius to a string -02.3
-    pub fn format(&self) -> [u8; 5] {
-        let mut result = [20u8; 5];
-        result[0] = if self.0 < 0 { '-' as u8 } else { ' ' as u8 };
-        let temp: u8 = self.0.abs() as u8;
-        result[1] = '0' as u8 + (temp / 160);
-        result[2] = '0' as u8 + (temp >> 4);
-        result[3] = '.' as u8;
-        //round fraction to one digit:
-        // 0	0.000
-        // 1	0.063
-        // 2	0.125
-        // 3	0.188
-        // 4	0.250
-        // 5	0.313
-        // 6	0.375
-        // 7	0.438
-        // 8	0.500
-        // 9	0.563
-        // 10	0.625
-        // 11	0.688
-        // 12	0.750
-        // 13	0.813
-        // 14	0.875
-        // 15	0.938
-        let table: &[u8] = b"0112334456678899";
-        result[4] = table[(temp & 0xf) as usize];
-        result
+    pub fn fraction_degrees(&self) -> u8 {
+        (self.0.abs() & 0xF) as u8
     }
 }
 
